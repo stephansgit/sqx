62894a8a8c33cd7caef66e0624c22d28713a6079
Stephan Sprenger <stephan.sprenger@gmx.de>
2015-03-02 21:41:50 +0100

    updated Output-file

--- 0_boersentacho.sh
+++ 0_boersentacho.sh
@@ -6,7 +6,7 @@ Rscript /home/fibo/scripts/Boersentacho/1_Import_Calc.R
 echo "----------------------------------------"
 echo "Import fertig, starte Out-Generierung"
 echo "----------------------------------------"
-Rscript -e  "library(rmarkdown); render('/home/fibo/scripts/Boersentacho/3_Output.Rmd')"
+Rscript -e  "rmarkdown::render('/home/fibo/scripts/Boersentacho/3_Output.Rmd')"
 sudo mv /home/fibo/scripts/Boersentacho/3_Output.html /var/www/html/hbt.html
 echo "--------------"
 echo "Fertig"
