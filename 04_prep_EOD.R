346f005af2305def952305785ecd221a51a9464e
Sprenger <s6536@uniper.energy>
2022-06-04 19:55:21 +0200

    split the load_data process also in two in the makefile

--- 04_prep_EOD.R
+++ 04_prep_EOD.R
@@ -1,10 +1,10 @@
 #############################
-# Lädt eod-Daten von yahoo.
-# 26.07.2015
+# Präpariert die daten
+# 04.06.2022
 ##########################
 
 args <- commandArgs(trailingOnly = TRUE)
 
 source("01_functions.R")
 
-load_EoD_data(daten = args[1], output_eod = args[2])
+prep_EoD_data(daten = args[1], output_eod = args[2])
